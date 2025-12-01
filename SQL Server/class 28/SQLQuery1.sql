7c0bf5e89097536575181e89d1f0fed689ab2443
UMAIR <154393500+umair7228@users.noreply.github.com>
2025-12-01 22:59:04 +0500

    class 28

---
+++ SQL Server/class 28/SQLQuery1.sql
@@ -0,0 +1,293 @@
+SELECT * FROM sales.customers;
+
+SELECT * FROM sales.staffs;
+
+SELECT first_name, last_name
+FROM sales.staffs
+UNION
+SELECT first_name, last_name
+FROM sales.customers;
+
+SELECT COUNT(*)
+FROM sales.staffs
+
+SELECT COUNT(*)
+FROM sales.customers
+
+SELECT first_name, last_name
+FROM sales.staffs
+UNION ALL
+SELECT first_name, last_name
+FROM sales.customers;
+
+SELECT
+    city
+FROM
+    sales.customers
+INTERSECT
+SELECT
+    city
+FROM
+    sales.stores
+ORDER BY
+    city;
+
+SELECT product_id
+FROM production.products
+EXCEPT
+SELECT product_id
+FROM sales.order_items
+
+SELECT *
+FROM (
+	SELECT *
+	FROM (
+		SELECT
+			first_name + ' ' + last_name AS full_name,
+			YEAR(o.order_date) AS year,
+			SUM(list_price * quantity * (1 - discount)) AS total_sales
+		FROM sales.orders o
+		INNER JOIN sales.order_items ot ON o.order_id = ot.order_id
+		INNER JOIN sales.staffs s ON o.staff_id = s.staff_id
+		GROUP BY first_name + ' ' + last_name, YEAR(o.order_date)
+	) t
+	WHERE year = 2016
+) t1
+
+select * from sales.order_items;
+
+WITH sales_cte(staff, year, total_sales) AS (
+	SELECT
+		first_name + ' ' + last_name,
+		YEAR(o.order_date),
+		SUM(list_price * quantity * (1 - discount))
+	FROM sales.orders o
+	INNER JOIN sales.order_items ot ON o.order_id = ot.order_id
+	INNER JOIN sales.staffs s ON o.staff_id = s.staff_id
+	GROUP BY first_name + ' ' + last_name, YEAR(o.order_date)
+),
+filtered_cte AS (
+SELECT staff, year from sales_cte
+WHERE year = 2017)
+
+select * from filtered_cte;
+
+WITH num_cte(n, weekday) AS (
+	SELECT
+		0,
+		DATENAME(DW, 0)
+	UNION ALL
+	SELECT
+		n + 1,
+		DATENAME(DW, n + 1)
+	FROM num_cte
+	WHERE n < 6
+)
+SELECT *
+FROM num_cte;
+
+SELECT       
+    staff_id, 
+    first_name,
+    manager_id
+        
+FROM       
+    sales.staffs
+WHERE manager_id IS NULL
+
+WITH cte_org AS (
+    SELECT       
+        staff_id, 
+        first_name,
+        manager_id
+        
+    FROM       
+        sales.staffs
+    WHERE manager_id IS NULL
+    UNION ALL
+    SELECT 
+        e.staff_id, 
+        e.first_name,
+        e.manager_id
+    FROM 
+        sales.staffs e
+        INNER JOIN cte_org o 
+            ON o.staff_id = e.manager_id
+)
+SELECT * FROM cte_org;
+
+
+SELECT * FROM   
+(
+    SELECT 
+        category_name, 
+        product_id,
+		model_year
+    FROM 
+        production.products p
+        INNER JOIN production.categories c 
+            ON c.category_id = p.category_id
+) t
+PIVOT (
+	COUNT(product_id)
+	FOR category_name IN (
+		[Children Bicycles], 
+        [Comfort Bicycles], 
+        [Cruisers Bicycles], 
+        [Cyclocross Bicycles], 
+        [Electric Bikes], 
+        [Mountain Bikes], 
+        [Road Bikes]
+	)
+) AS pivot_table;
+
+CREATE TABLE sales.taxes (
+	tax_id INT PRIMARY KEY IDENTITY (1, 1),
+	state VARCHAR (50) NOT NULL UNIQUE,
+	state_tax_rate DEC (3, 2),
+	avg_local_tax_rate DEC (3, 2),
+	combined_rate AS state_tax_rate + avg_local_tax_rate,
+	max_local_tax_rate DEC (3, 2),
+	updated_at datetime
+);
+
+SELECT * FROM sales.taxes;
+
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Alabama',0.04,0.05,0.07);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Alaska',0,0.01,0.07);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Arizona',0.05,0.02,0.05);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Arkansas',0.06,0.02,0.05);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('California',0.07,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Colorado',0.02,0.04,0.08);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Connecticut',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Delaware',0,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Florida',0.06,0,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Georgia',0.04,0.03,0.04);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Hawaii',0.04,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Idaho',0.06,0,0.03);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Illinois',0.06,0.02,0.04);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Indiana',0.07,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Iowa',0.06,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Kansas',0.06,0.02,0.04);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Kentucky',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Louisiana',0.05,0.04,0.07);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Maine',0.05,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Maryland',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Massachusetts',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Michigan',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Minnesota',0.06,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Mississippi',0.07,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Missouri',0.04,0.03,0.05);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Montana',0,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Nebraska',0.05,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Nevada',0.06,0.01,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('New Hampshire',0,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('New Jersey',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('New Mexico',0.05,0.02,0.03);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('New York',0.04,0.04,0.04);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('North Carolina',0.04,0.02,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('North Dakota',0.05,0.01,0.03);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Ohio',0.05,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Oklahoma',0.04,0.04,0.06);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Oregon',0,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Pennsylvania',0.06,0,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Rhode Island',0.07,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('South Carolina',0.06,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('South Dakota',0.04,0.01,0.04);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Tennessee',0.07,0.02,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Texas',0.06,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Utah',0.05,0,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Vermont',0.06,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Virginia',0.05,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Washington',0.06,0.02,0.03);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('West Virginia',0.06,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Wisconsin',0.05,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Wyoming',0.04,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('D.C.',0.05,0,0);
+
+SELECT * FROM sales.taxes;
+
+UPDATE sales.taxes
+SET updated_at = GETDATE();
+
+UPDATE sales.taxes
+SET max_local_tax_rate += 0.02
+
+UPDATE sales.taxes
+SET state_tax_rate = 0.04
+WHERE tax_id = 2;
+
+DROP TABLE IF EXISTS sales.targets;
+
+CREATE TABLE sales.targets
+(
+    target_id  INT	PRIMARY KEY, 
+    percentage DECIMAL(4, 2) 
+        NOT NULL DEFAULT 0
+);
+
+INSERT INTO 
+    sales.targets(target_id, percentage)
+VALUES
+    (1,0.2),
+    (2,0.3),
+    (3,0.5),
+    (4,0.6),
+    (5,0.8);
+
+SELECT * FROM sales.targets;
+
+CREATE TABLE sales.commissions
+(
+    staff_id    INT PRIMARY KEY, 
+    target_id   INT, 
+    base_amount DECIMAL(10, 2) 
+        NOT NULL DEFAULT 0, 
+    commission  DECIMAL(10, 2) 
+        NOT NULL DEFAULT 0, 
+    FOREIGN KEY(target_id) 
+        REFERENCES sales.targets(target_id), 
+    FOREIGN KEY(staff_id) 
+        REFERENCES sales.staffs(staff_id),
+);
+
+INSERT INTO 
+    sales.commissions(staff_id, base_amount, target_id)
+VALUES
+    (1,100000,2),
+    (2,120000,1),
+    (3,80000,3),
+    (4,900000,4),
+    (5,950000,5);
+
+SELECT * FROM sales.targets
+SELECT * FROM sales.commissions
+
+UPDATE
+    sales.commissions
+SET
+    sales.commissions.commission = 
+        c.base_amount * t.percentage
+FROM 
+    sales.commissions c
+    INNER JOIN sales.targets t
+        ON c.target_id = t.target_id;
+
+SELECT * 
+INTO production.product_history
+FROM
+    production.products;
+
+SELECT * FROM production.product_history;
+
+DELETE TOP (20)
+FROM production.product_history;
+
+select * from production.product_history;
+
+DELETE TOP (5) PERCENT
+FROM production.product_history;
+
+DELETE FROM production.product_history;
+
+DROP TABLE production.product_history;
